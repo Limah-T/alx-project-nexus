34eb4494937fb388a1597a773aeb9dcf711b1f5a
Limah-T <limahtechnology@yahoo.com>
2025-09-28 19:27:00 +0100

    Debug an error to production.

--- api/views.py
+++ api/views.py
@@ -491,7 +491,7 @@ class PaymentView(APIView):
             if not check_product_quantity(cart, request.data):
                 return Response({"error": "Product quantity is higher than stock quantity"}, status=400)
             payment_transaction = initializeTransaction(product_data=request.data)
-            reference = payment_transaction["data"]["reference"]
+        reference = payment_transaction["data"]["reference"]
         payment, created = Payment.objects.get_or_create(
                                 cart=check_if_products_exist_in_cart(cart), 
                                 defaults={"reference": reference}
